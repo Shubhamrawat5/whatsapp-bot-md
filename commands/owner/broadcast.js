25a769eec105d383f6e7bdf9ea5b9b03d50488a8
Shubham Rawat <sr856161@gmail.com>
2022-07-15 12:53:20 +0530

    fixing broadcast command

--- commands/owner/broadcast.js
+++ commands/owner/broadcast.js
@@ -7,37 +7,47 @@ module.exports.command = () => {
 };
 
 const handler = async (sock, msg, from, args, msgInfoObj) => {
-  let { prefix } = msgInfoObj;
+  let { prefix, reply } = msgInfoObj;
   let chats = await sock.groupFetchAllParticipating();
   // console.log(chats);
   // !v.announce &&
-  let groups = Object.values(chats).filter((v) =>
-    v.id.endsWith("g.us").map((v) => {
+  let groups = Object.values(chats)
+    .filter((v) => v.id.endsWith("g.us"))
+    .map((v) => {
       return { subject: v.subject, id: v.id };
-    })
-  );
+    });
+  //  && v.subject.startsWith("<{PVX}>")
   // console.log(groups);
 
   let message = "Broadcast:\n";
   if (
     msg.message.extendedTextMessage &&
-    msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation
+    msg.message.extendedTextMessage.contextInfo &&
+    msg.message.extendedTextMessage.contextInfo.quotedMessage
   ) {
-    message +=
-      msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation;
+    console.log("true");
+    if (msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation)
+      message +=
+        msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation;
+    else
+      message +=
+        msg.message.extendedTextMessage.contextInfo.quotedMessage
+          .extendedTextMessage.text;
   } else {
     message += args.length ? args.join(" ") : "";
   }
 
-  if (msg == "Broadcast:\n") {
+  console.log(message == "Broadcast:\n");
+  if (message == "Broadcast:\n") {
     reply("❌ ERROR: EMPTY MESSAGE!");
     return;
   }
 
   let time = 0;
+  reply("Broadcasting...");
   groups.forEach((group) => {
     setTimeout(() => {
-      sock.sendMessage(group.id, { text: message });
+      sock.sendMessage(group.id, { text: message, detectLinks: true });
     }, time);
     time += 1000 * 30; //30 sec
   });
