9217546aa995a7ffb197be4acfb232dc981c3457
Shubham Rawat <sr856161@gmail.com>
2022-08-16 19:30:40 +0530

    dd: delete bot message also

--- commands/group/admins/dd.js
+++ commands/group/admins/dd.js
@@ -9,9 +9,19 @@ module.exports.command = () => {
 const handler = async (sock, msg, from, args, msgInfoObj) => {
   let { botNumberJid, reply } = msgInfoObj;
 
+  if (!msg.message.extendedTextMessage) {
+    reply("❌ Tag message to delete.");
+    return;
+  }
+
+  let fromBot = false;
+  if (msg.message.extendedTextMessage.contextInfo.participant == botNumberJid) {
+    fromBot = true;
+  }
+
   const options = {
     remoteJid: from,
-    fromMe: false,
+    fromMe: fromBot,
     id: msg.message.extendedTextMessage.contextInfo.stanzaId,
     participant: msg.message.extendedTextMessage.contextInfo.participant,
   };
