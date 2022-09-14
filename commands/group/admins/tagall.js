9ccd18523d83691d4945b1fc8ef4928b832dc50f
Shubham Rawat <sr856161@gmail.com>
2022-09-14 14:49:26 +0530

    make tagall for admins

---
+++ commands/group/admins/tagall.js
@@ -0,0 +1,46 @@
+const { MessageType, Mimetype } = require("@adiwajshing/baileys");
+
+module.exports.command = () => {
+  let cmd = ["tagall"];
+
+  return { cmd, handler };
+};
+
+const handler = async (sock, msg, from, args, msgInfoObj) => {
+  let {
+    sender,
+    prefix,
+    groupName,
+    groupMembers,
+    botNumberJid,
+    myNumber,
+    reply,
+  } = msgInfoObj;
+  //if (
+  //  groupName.toUpperCase().includes("PVX") &&
+  //  ![myNumber + "@s.whatsapp.net", botNumberJid].includes(sender)
+  //) {
+  //  reply(`❌ Owner only command for avoiding spam in PVX Groups!`);
+  //  return;
+  //}
+
+  let jids = [];
+  let message = "ALL: ";
+  if (
+    msg.message.extendedTextMessage &&
+    msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation
+  ) {
+    message +=
+      msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation +
+      "\n\n";
+  } else {
+    message += args.length ? args.join(" ") + "\n\n" : "";
+  }
+
+  for (let i of groupMembers) {
+    message += "@" + i.id.split("@")[0] + " ";
+    jids.push(i.id.replace("c.us", "s.whatsapp.net"));
+  }
+
+  sock.sendMessage(from, { text: message, mentions: jids }, { quoted: msg });
+};
