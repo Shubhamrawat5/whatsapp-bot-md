6d7f9500205bef80d4c1e99ee721ff266a88708a
Shubham Rawat <sr856161@gmail.com>
2023-01-19 00:50:59 +0530

    timeout for forward sticker

--- functions/forwardSticker.js
+++ functions/forwardSticker.js
@@ -5,39 +5,49 @@ const { LoggerTg } = require("./loggerBot");
 const pvxstickeronly1 = "919557666582-1628610549@g.us";
 const pvxstickeronly2 = "919557666582-1586018947@g.us";
 let countSent = 1;
+let countIn = 0,
+  countOut = 0,
+  countErr = 0;
 
 module.exports.forwardSticker = async (bot, msg) => {
   try {
+    countIn += 1;
     let downloadFilePath = msg.message.stickerMessage;
-    const stream = await downloadContentFromMessage(
-      downloadFilePath,
-      "sticker"
-    );
+    let stream = await downloadContentFromMessage(downloadFilePath, "sticker");
     if (!stream) return;
-    
+
     let buffer = Buffer.from([]);
     for await (const chunk of stream) {
       buffer = Buffer.concat([buffer, chunk]);
     }
 
-    const sticker = new Sticker(buffer, {
+    let sticker = new Sticker(buffer, {
       pack: "BOT 🤖",
       author: "pvxcommunity.com",
       type: StickerTypes.DEFAULT,
       quality: 80,
     });
 
-    await bot.sendMessage(pvxstickeronly1, await sticker.toMessage(), {
+    let stickerMesssage = await sticker.toMessage();
+
+    //1000*60*60*24 = 86400ms = 1 day
+    await bot.sendMessage(pvxstickeronly1, stickerMesssage, {
       ephemeralExpiration: 86400,
+      mediaUploadTimeoutMs: 1000 * 20,
     });
-    await bot.sendMessage(pvxstickeronly2, await sticker.toMessage(), {
+    await bot.sendMessage(pvxstickeronly2, stickerMesssage, {
       ephemeralExpiration: 86400,
+      mediaUploadTimeoutMs: 1000 * 20,
     });
 
-    console.log(`${countSent} sticker sent!`);
+    countOut += 1;
+    console.log(
+      `${countSent} sticker sent! In:${countIn}, Out:${countOut}, Err:${countErr}`
+    );
     countSent += 1;
   } catch (err) {
     // await LoggerBot(false, "FORWARD-STICKER", err, undefined);
     await LoggerTg(`ERROR: [FORWARD-STICKER]\n${err.toString()}`);
+    countErr += 1;
   }
 };
