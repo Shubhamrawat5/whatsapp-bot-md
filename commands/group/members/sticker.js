6fec65e3ecffb0e4f4dfcab94f9e7affc94b22a6
Shubham Rawat <sr856161@gmail.com>
2023-02-20 17:35:12 +0530

    user given quality for sticker

--- commands/group/members/sticker.js
+++ commands/group/members/sticker.js
@@ -18,14 +18,35 @@ const getRandom = (ext) => {
   return `${Math.floor(Math.random() * 10000)}${ext}`;
 };
 
+const getQuality = (isCrop, args1, args2) => {
+  let quality;
+  if (!isCrop && args1 && !isNaN(Number(args1))) {
+    //1st arg check
+    quality = Number(args1);
+  }
+  if (isCrop && args2 && !isNaN(Number(args2))) {
+    //2nd arg check
+    quality = Number(args2);
+  }
+
+  return quality;
+};
+
 const handler = async (bot, msg, from, msgInfoObj) => {
   let { type, isTaggedImage, isTaggedVideo, reply, args } = msgInfoObj;
   let packName = "BOT 🤖";
   let authorName = "pvxcommunity.com";
+  let quality;
+
+  const args1 = args[0];
+  const args2 = args[1];
+  const isCrop = args1 === "c" || args1 === "crop";
+
   const stickerFileName = getRandom(".webp");
   let stickerMake;
   //for image
   if (type === "imageMessage" || isTaggedImage) {
+    quality = 100;
     let downloadFilePath;
     if (msg.message.imageMessage) {
       downloadFilePath = msg.message.imageMessage;
@@ -38,17 +59,19 @@ const handler = async (bot, msg, from, msgInfoObj) => {
     const stream = await downloadContentFromMessage(downloadFilePath, "image");
     const buffer = await toBuffer(stream);
 
+    const resQuality = getQuality(isCrop, args1, args2);
+    if (resQuality) quality = resQuality;
+    console.log("Sticker Quality: ", quality);
+
     stickerMake = new Sticker(buffer, {
       pack: packName,
       author: authorName,
-      type:
-        args.includes("crop") || args.includes("c")
-          ? StickerTypes.CROPPED
-          : StickerTypes.FULL,
-      quality: 100,
+      type: isCrop ? StickerTypes.CROPPED : StickerTypes.FULL,
+      quality: quality,
     });
   } else if (type === "videoMessage" || isTaggedVideo) {
     //for videos
+    quality = 40;
     let downloadFilePath;
     if (msg.message.videoMessage) {
       downloadFilePath = msg.message.videoMessage;
@@ -59,14 +82,15 @@ const handler = async (bot, msg, from, msgInfoObj) => {
     const stream = await downloadContentFromMessage(downloadFilePath, "video");
     const buffer = await toBuffer(stream);
 
+    const resQuality = getQuality(isCrop, args1, args2);
+    if (resQuality) quality = resQuality;
+    console.log("Sticker Quality: ", quality);
+
     stickerMake = new Sticker(buffer, {
       pack: packName, // The pack name
       author: authorName, // The author name
-      type:
-        args.includes("crop") || args.includes("c")
-          ? StickerTypes.CROPPED
-          : StickerTypes.FULL,
-      quality: 40,
+      type: isCrop ? StickerTypes.CROPPED : StickerTypes.FULL,
+      quality: quality,
     });
   } else {
     await reply("❌ Give a media to convert into sticker!");
