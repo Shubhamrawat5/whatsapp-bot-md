22f40ff0c685abd68e5c20ea5d253707ed6690ab
Shubham Rawat <sr856161@gmail.com>
2023-03-30 11:57:24 +0530

    add movie command

---
+++ commands/group/members/movie.js
@@ -0,0 +1,15 @@
+const importDynamic = new Function("modulePath", "return import(modulePath)");
+require("dotenv").config();
+let api;
+let isApiSetup = false;
+
+module.exports.command = () => {
+  return { cmd: ["movie"], handler: handler };
+};
+
+const handler = async (bot, msg, from, msgInfoObj) => {
+  let { reply } = msgInfoObj;
+
+  const text = `Join PVX TG group:\n\nhttps://t.me/joinchat/J7FzKB1uYt0xNDVl`;
+  reply(text);
+};
