e8d23b67c2e2fc8535654132a4adaab2da7a1327
Shubham Rawat <sr856161@gmail.com>
2023-04-05 21:23:43 +0530

    add rule 10

--- commands/group/members/rules.js
+++ commands/group/members/rules.js
@@ -18,7 +18,7 @@ _Do not spam in the grp._
 _Do not send any inappropriate content in the grp._
 
 ✔ Rule 03 -
-_Be Active. You should've sent atleast 100 messages in a month. [Your messages are being counted daily by the bot.]_
+_Be Active. You should've sent atleast 100 messages in a month. [Your messages are being counted daily by the bot]_
 
 ✔ Rule 04 -
 _Do not swear on someone else's parents  just because they roasted you badly._
@@ -37,6 +37,9 @@ _Do not give any spoilers of any new movie or series._
 
 ✔ Rule 09 -
 _Only numbers starting with the code +91 (i.e. Indians) are allowed to join._
+
+✔ Rule 10 -
+_Do any transaction with any member at your own risk._
 
 *You may get a warning or ban if you're found breaking any of the above rules. PVX is a growing community, we'll appreciate if you follow these rules and help us grow more.* ❤️`;
 
